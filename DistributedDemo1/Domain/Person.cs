a53a2a6368eb0197c4a18936881f7b8400119f6a
masirg <masirg@ttu.ee>
2019-02-04 09:39:17 +0200

    init

---
+++ DistributedDemo1/Domain/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        [MaxLength(64)]
+        [MinLength(1)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(64)]
+        [MinLength(1)]
+        [Required]
+        public string LastName { get; set; }
+    }
+}
